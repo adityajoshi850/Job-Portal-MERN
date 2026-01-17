e9348a9b174a92d99cae35407180e6edf073ea13
Adi <joshi55aditya@gmail.com>
2026-01-17 23:21:00 +0545

    Fix formatting and spacing in model relationships; improve code readability

--- model/index.js
+++ model/index.js
@@ -4,16 +4,16 @@ const User = require("./UserModel");
 
 
 // Relationships between User and Job
-User.hasMany(Job, { foreignKey: 'userId'});
-Job.belongsTo(User, { foreignKey: 'userId'});
+User.hasMany(Job, { foreignKey: 'userId' });
+Job.belongsTo(User, { foreignKey: 'userId' });
 
 // Relationship between Application and User
-User.hasMany(Application, {foreignKey: 'userId'})
-Application.belongsTo(User, {foreignKey: 'userId'});
+User.hasMany(Application, { foreignKey: 'userId' })
+Application.belongsTo(User, { foreignKey: 'userId' });
 
 // Relatiionship betweeen Application and job
-Job.hasMany(Application, {foreignKey: "jobId"})
-Application.belongsTo(Job, {foreignKey: "jobId"})
+Job.hasMany(Application, { foreignKey: "jobId" })
+Application.belongsTo(Job, { foreignKey: "jobId" })
 
 
-module.exports = {User, Job}
+module.exports = { User, Job }
